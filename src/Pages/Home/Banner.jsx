987d2c94dde0fa3c7b536a992c97f8b291a8a15d
fakeistiak <istiakahmedb100.com>
2024-02-14 18:01:13 +0600

    package added

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -2,7 +2,7 @@
 const Banner = () => {
     return (
         <div>
-            <h1 className="text-5xl font-bold">Hello World</h1>
+            <h1 className="text-5xl font-bold text-red-500">Hello World</h1>
         </div>
     );
 };
