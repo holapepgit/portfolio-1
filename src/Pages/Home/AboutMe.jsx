df6a9ca07345d7714d009f64f2025b8ab811a9ee
fakeistiak <istiakahmedb100.com>
2024-02-26 13:38:53 +0600

    Projects edited

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -3,8 +3,8 @@ const AboutMe = () => {
     <>
       <div className=" px-6 py-16 mx-auto bg-white">
         <div className="lg:mx-auto text-center">
-          <h2 className="text-3xl font-bold mb-2">About</h2>
-          <div className="mx-auto w-20 h-1 mb-5 bg-secondary"></div>
+          <h2 className="text-3xl font-bold mb-2">Background</h2>
+          <div className="mx-auto w-48 h-1 mb-5 bg-secondary"></div>
         </div>
         <div className="items-center lg:flex justify-evenly">
           <div className="flex items-center justify-center w-full mt-6 lg:mt-0  lg:shadow-xl lg:border-4 lg:border-black lg:w-[300px] lg:h-[400px]">
@@ -17,20 +17,19 @@ const AboutMe = () => {
           <div className="w-full lg:w-1/2">
             <div className="lg:max-w-lg">
               <p className="pt-6 text-gray-700 ">
-                Hey, My name is Garv Nanwani and I am currently a Second Year
-                Undergraduate Student at Jamia Millia Islamia, New Delhi.
+                • The South East And South-South Nigeria are in strategic
+                locations in the Country.
               </p>
               <p className="pt-6 text-gray-700 ">
-                My field of Interests are programming and psychology.
+                The combined regions population is estimated to be 65M.
               </p>
               <p className="pt-6 text-gray-700 ">
-                I have been coding for around 2 years now. I love to design
-                websites and build full stack applications using MERN stack
-                preferably.
+                • Nigeria imports over 131 million tons of cargo each year.
               </p>
               <p className="pt-6 text-gray-700 ">
-                I am also a guest writer at daily.dev where I write technical
-                articles related to development and programming in general.
+                • It is estimated that the transregional corridor through this
+                rail project has the potential to transport 50 million-100
+                million tons of cargo each year.
               </p>
 
               <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-gray-600 rounded-lg lg:w-auto hover:bg-gray-500 focus:outline-none focus:bg-gray-500">
--- src/Pages/Home/Projects.jsx
+++ src/Pages/Home/Projects.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   const [projectData, setProjectData] = useState([]);
@@ -20,7 +21,8 @@ const Projects = () => {
         {projectData.map((project, index) => (
           <div
             key={index}
-            className="w-full max-w-xl overflow-hidden bg-gray-100 rounded-lg shadow-lg">
+            className="w-full max-w-xl overflow-hidden bg-gray-100 rounded-lg shadow-lg"
+          >
             <img
               className="object-cover w-full h-72"
               src={project.image}
@@ -29,9 +31,20 @@ const Projects = () => {
             <div className="py-5 text-center">
               <h1 className="block text-xl font-bold">{project.name}</h1>
               <p className="text-sm p-2">{project.description}</p>
+              <Link className="w-full px-3 py-2 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-gray-600 rounded-lg lg:w-auto hover:bg-gray-500 focus:outline-none focus:bg-gray-500 cursor-pointer">
+                Show details
+              </Link>
             </div>
           </div>
         ))}
+      </div>
+      <div className="flex justify-center items-center text-center lg:my-6 my-3">
+        <Link
+          to="details"
+          className="w-full px-5 py-2 mt-6 text-sm tracking-wider uppercase transition-colors duration-300 transform bg-transparent text-black font-bold rounded-lg lg:w-auto hover:bg-gray-200 focus:outline-black focus:bg-gray-200 border border-black"
+        >
+          Show more
+        </Link>
       </div>
     </div>
   );
