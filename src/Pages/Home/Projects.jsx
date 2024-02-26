bf630343c35607cacf4314cf4c4d580be7e8be9e
fakeistiak <istiakahmedb100.com>
2024-02-26 13:03:09 +0600

    update

--- src/Pages/Home/Projects.jsx
+++ src/Pages/Home/Projects.jsx
@@ -10,32 +10,6 @@ const Projects = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-<<<<<<< HEAD
-    return (
-      <div className="bg-white">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 py-8 md:py-12 lg:py-20 max-w-6xl mx-auto gap-6 md:gap-8 lg:gap-20">
-          {projectData.map((project, index) => (
-            <div
-              key={index}
-              className="w-full max-w-xs sm:max-w-md md:max-w-xl overflow-hidden bg-gray-100 rounded-lg shadow-lg"
-            >
-              <img
-                className="object-cover w-full h-48 sm:h-56 md:h-64"
-                src={project.image}
-                alt="project avatar"
-              />
-              <div className="py-4 px-6 md:px-8 lg:px-10">
-                <h1 className="block text-lg sm:text-xl md:text-2xl font-bold leading-tight mb-2">
-                  {project.name}
-                </h1>
-                <p className="text-sm sm:text-base">{project.description}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-=======
   return (
     <div className="bg-white py-20 px-5 lg:px-0">
       <div className="lg:mx-auto text-center ">
@@ -61,7 +35,6 @@ const Projects = () => {
       </div>
     </div>
   );
->>>>>>> bcdab7f246e4ce1da7ddd08edbdffcd7b633dd54
 };
 
 export default Projects;
