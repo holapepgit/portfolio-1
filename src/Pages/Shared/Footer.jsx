718c215d3a4f516f20574aa6568a5fe916dd550a
fakeistiak <istiakahmedb100.com>
2024-02-27 19:52:05 +0600

    Footer edited

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,31 +1,16 @@
-import {
-  FaDev,
-  FaGithub,
-  FaInstagram,
-  FaLinkedin,
-  FaTwitter,
-} from "react-icons/fa";
+import { FaPhone, FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <div className=" bg-primary py-4">
-      <div className="text-2xl space-x-8 flex  text-white justify-center py-6">
+      <div className="text-2xl space-x-6 flex text-white justify-center py-6 items-center">
         <a href="" className="k">
-          <FaTwitter />
+          <FaWhatsapp className="text-4xl" />
         </a>
         <a href="" className="k">
-          <FaLinkedin />
-        </a>
-        <a href="" className="k">
-          <FaGithub />
-        </a>
-        <a href="" className="k">
-          <FaDev />
-        </a>
-        <a href="" className="k">
-          <FaInstagram />
+          <FaPhone className="rotate-90 text-2xl" />
         </a>
       </div>
       <h1 className="text-gray-200 text-lg text-center font-medium ">
